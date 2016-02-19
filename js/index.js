dd7e7304d45cdd624d09744cb0d12086ae7d7935
Gerard Gust <zod799@users.noreply.github.com>
2016-02-18 16:07:00 -0800

    uploaded files from CodePen

---
+++ js/index.js
@@ -0,0 +1,69 @@
+'use strict';
+
+/* var formFunctions = {
+    init: function(){
+        this.placeholder();
+    },
+    placeholder: function(elem){
+        $('body').on('focusin', '.float-labels input', function(e){
+            if(!$(this).attr('readonly') && $(this).attr('type') != "radio"){
+                formFunctions.addFloatLabel($(this));
+            }
+        });
+        $('body').on('focusout', '.float-labels input', function(e){
+            if(!$(this).val()){
+                formFunctions.removeFloatLabel($(this));
+            }
+        });
+        $('body').on('change', '.float-labels input', function(e){
+            if($(this).attr('type') != "radio"){
+                formFunctions.addFloatLabel($(this));
+            }
+        });
+    },
+    addFloatLabel: function (elem) {
+        elem.parent().addClass('f-label-active');
+    },
+    removeFloatLabel: function (elem) {
+        elem.parent().removeClass('f-label-active');
+    }
+}
+formFunctions.init(); */
+
+/* =============== Float Labels for Angular ======================  
+   ADD 'float-labels' class to the form that needs float labels
+*/
+var app = angular.module('app', []);
+app.directive('floatLabels', function ($timeout) {
+    return {
+        restrict: 'C',
+        link: function link(scope, element, attrs) {
+
+            $timeout(function () {
+                var inputs = element[0].getElementsByTagName('input');
+
+                for (var i = 0; i < inputs.length; i++) {
+                    inputs[i].value ? addFloatLabel(inputs[i]) : "";
+
+                    $(inputs[i]).on('focusin focusout change', setEvents);
+                }
+
+                function setEvents(e) {
+                    if (e.type == 'focusin' && this.getAttribute('readonly') != "readonly" && this.getAttribute('type') != "radio" || e.type == 'change' && this.getAttribute('type') != "radio") {
+                        addFloatLabel(this);
+                    } else if (e.type == 'focusout' && !this.value) {
+                        removeFloatLabel(this);
+                    }
+                }
+                function addFloatLabel(elem) {
+                    parent = elem.parentNode;
+                    parent.classList.add('f-label-active');
+                }
+                function removeFloatLabel(elem) {
+                    parent = elem.parentNode;
+                    parent.classList.remove('f-label-active');
+                }
+            });
+        }
+    };
+});
